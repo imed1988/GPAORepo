229d39724d7e8defd17392ca8614423b1c465c58
benzarti <benzimededdine@hotmail.com>
2018-11-13 13:40:36 +0100

    Changes In GPAO

---
+++ GPAO/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using GPAO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GPAO.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult ViewAll()
+        {
+            return View(GetAllUsers());
+        }
+
+        IEnumerable<User> GetAllUsers()
+        {
+            using (GPAOEntities db = new GPAOEntities())
+            {
+
+                return db.User.ToList<User>();
+            }
+        }
+    }
+}
